80b7cc77b95fc8444580f688dd4e542591e2f24e
Giorgi Kapanadze <giorgikapanadze222@gmail.com>
2025-11-03 12:38:17 +0400

    feat(api): add health endpoint

--- backend/revalyt/urls.py
+++ backend/revalyt/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .health import health
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/analytics/", include("analytics.urls")),
     path("api/auth/", include("users.urls")),
+    path("health/", health, name="health"),
 ]
