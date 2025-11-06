feefd0859f58bd2ecf8bbe0379a5f8f1fb398916
Giorgi Kapanadze <giorgikapanadze222@gmail.com>
2025-11-06 13:33:30 +0400

    fix api proxy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://revalytiq-backend.onrender.com/api/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
